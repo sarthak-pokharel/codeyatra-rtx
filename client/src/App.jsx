a4384fdcdc19fc2d37e874a5f80bd5fed39d396a
Sanandan Ghimire <sanandanghimire6688@gmail.com>
2025-02-09 15:30:32 +0545

    2nd

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,12 +1,27 @@
-import { useState } from "react";
-
+import { Routes, BrowserRouter as Router, Route } from "react-router-dom";
 import "./App.css";
+import Home from "./Components/Home";
+import Navbar from "./Components/Navbar";
+import Login from "./Components/Login";
+import Contact from "./Components/Contact";
+import About from "./Components/About";
+import Features from "./Components/Features";
 
 function App() {
   return (
-    <>
-      <p>hello world</p>
-    </>
+    <Router>
+      <div>
+        <Navbar />
+
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/contact" element={<Contact />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/features" element={<Features />} />
+          <Route path="/login" element={<Login />} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
